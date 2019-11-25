c37183ca282fd5722ee4da86e002c84f229f40f8
Slendercoder <andradeed78@gmail.com>
2019-11-25 18:25:20 -0500

    Buscando todavia

--- R codes/fitFRA_full.R
+++ R codes/fitFRA_full.R
@@ -3,53 +3,69 @@ source("FRApred_full.R")
 library(dfoptim)
 library(beepr)
 
-df1 = read.csv("../Python Codes/humans.csv")
-head(df1)
-getFreqFromGameFRA(df1)
+#df1 = read.csv("../Python Codes/humans.csv")
+#head(df1)
+#getFreqFromGameFRA(df1)
 
-data = read.csv("frequencies.csv")
-head(data)
+#data = read.csv("frequencies.csv")
+#head(data)
 
-args <- getArgs(data, regiones)
-args <- args[order(-args$s, args$i),] 
+#args <- getArgs(data, regiones)
+#args <- args[order(-args$s, args$i),] 
+#write.csv(args, file = "args.csv", row.names = FALSE)
+#print('Args written in args.csv!')
+
+args = read.csv("args.csv")
 #beep()
 #head(args)
 #dim(args)
 
 # To search for best parameters FRA model
-wAll <- 0.1 # bias All
-wNoth <- 0.1 # bias Nothing
-wLef <- 0.1 # bias Left, Right, Top, Bottom
-wIn <- 0.1 # bias In, Out
+wAll <- 0.12 # bias All
+wNoth <- 0.12 # bias Nothing
+wLef <- 0.12 # bias Left, Right, Top, Bottom
+wIn <- 0.12 # bias In, Out
 w2 <- 10 # win stay 
-w3 <- 0.05 # delta
-w4 <- 0.5 # zeta
-fitresFRA <- nmkb(par=c(wAll, wNoth, wLef, wIn, w2, w3, w4),
+w3 <- 10 # steepness of sigmoid 
+w4 <- 10 # threshold of sigmoid 
+w5 <- 1 # delta
+w6 <- 1 # exponential sim to complement
+w7 <- 1 # zeta
+w8 <- 1 # exponential sim to region
+fitresFRA <- nmkb(par=c(wAll, wNoth, wLef, wIn, w2, w3, w4, w5, w6, w7, w8),
                    fn = function(theta) FRAutil(c(theta[1],
                                                  theta[2],
                                                  theta[3],
                                                  theta[4],
                                                  theta[5],
-                                                 10, 
-                                                 31, 
                                                  theta[6], 
-                                                 1, 
                                                  theta[7], 
-                                                 1.2), args, regiones),
-                   lower=c(0,
-                           0,
-                           0,
-                           0,
-                           0,
-                           0,
-                           0),
+                                                 theta[8], 
+                                                 theta[9], 
+                                                 theta[10], 
+                                                 theta[11]), args, regiones),
+                   lower=c(0.1,
+                           0.1,
+                           0.1,
+                           0.1,
+                           8,
+                           8,
+                           8,
+                           0.9,
+                           0.5,
+                           0.9,
+                           0.5),
                    upper=c(0.15,
                            0.15,
                            0.15,
                            0.15,
                            200,
+                           30,
+                           30,
                            15,
-                           10),
+                           2,
+                           10,
+                           2),
                    control=list(trace=0))
 
 beep()
