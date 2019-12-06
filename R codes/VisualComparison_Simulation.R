8e4d3e1bb5593f169cedd73854bc89b2032b7ea6
Slendercoder <andradeed78@gmail.com>
2019-12-06 14:32:00 -0500

    Finite size analysis finished

--- R codes/VisualComparison_Simulation.R
+++ R codes/VisualComparison_Simulation.R
@@ -5,19 +5,19 @@ library(Rmisc)
 ###############################################
 # 1 dyad simulated
 ###############################################
-df1 = read.csv("../Python Codes/Sweeps/sim1_1.csv")
+df1 = read.csv("../Python Codes/Sweeps/Full/sim1_1.csv")
 df1$Exp <- as.character("1")
 head(df1)
-df2 = read.csv("../Python Codes/Sweeps/sim1_2.csv")
+df2 = read.csv("../Python Codes/Sweeps/Full/sim1_2.csv")
 df2$Exp <- as.character("2")
 head(df2)
-df3 = read.csv("../Python Codes/Sweeps/sim1_3.csv")
+df3 = read.csv("../Python Codes/Sweeps/Full/sim1_3.csv")
 df3$Exp <- as.character("3")
 head(df3)
-df4 = read.csv("../Python Codes/Sweeps/sim1_4.csv")
+df4 = read.csv("../Python Codes/Sweeps/Full/sim1_4.csv")
 df4$Exp <- as.character("4")
 head(df4)
-df5 = read.csv("../Python Codes/Sweeps/sim1_5.csv")
+df5 = read.csv("../Python Codes/Sweeps/Full/sim1_5.csv")
 df5$Exp <- as.character("5")
 head(df5)
 
@@ -59,19 +59,19 @@ g1
 ###############################################
 # 5 dyads simulated
 ###############################################
-df1 = read.csv("../Python Codes/Sweeps/sim5_1.csv")
+df1 = read.csv("../Python Codes/Sweeps/Full/sim5_1.csv")
 df1$Exp <- as.character("1")
 head(df1)
-df2 = read.csv("../Python Codes/Sweeps/sim5_2.csv")
+df2 = read.csv("../Python Codes/Sweeps/Full/sim5_2.csv")
 df2$Exp <- as.character("2")
 head(df2)
-df3 = read.csv("../Python Codes/Sweeps/sim5_3.csv")
+df3 = read.csv("../Python Codes/Sweeps/Full/sim5_3.csv")
 df3$Exp <- as.character("3")
 head(df3)
-df4 = read.csv("../Python Codes/Sweeps/sim5_4.csv")
+df4 = read.csv("../Python Codes/Sweeps/Full/sim5_4.csv")
 df4$Exp <- as.character("4")
 head(df4)
-df5 = read.csv("../Python Codes/Sweeps/sim5_5.csv")
+df5 = read.csv("../Python Codes/Sweeps/Full/sim5_5.csv")
 df5$Exp <- as.character("5")
 head(df5)
 
@@ -110,74 +110,21 @@ g5 <- ggplot(df, aes(DLIndex, colour=Exp, group=Exp)) +
 g5
 
 ###############################################
-# 10 dyads simulated
-###############################################
-df1 = read.csv("../Python Codes/Sweeps/sim10_1.csv")
-df1$Exp <- as.character("1")
-head(df1)
-df2 = read.csv("../Python Codes/Sweeps/sim10_2.csv")
-df2$Exp <- as.character("2")
-head(df2)
-df3 = read.csv("../Python Codes/Sweeps/sim10_3.csv")
-df3$Exp <- as.character("3")
-head(df3)
-df4 = read.csv("../Python Codes/Sweeps/sim10_4.csv")
-df4$Exp <- as.character("4")
-head(df4)
-df5 = read.csv("../Python Codes/Sweeps/sim10_5.csv")
-df5$Exp <- as.character("5")
-head(df5)
-
-# Create single data frame with DLIndexes
-df <- rbind(
-  df1[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df2[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df3[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df4[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df5[c('Round', 
-        'DLIndex',
-        'Exp')]
-)
-df$Exp <- as.factor(df$Exp)
-head(df)
-# levels(df$Exp)
-
-# Density plot
-g10 <- ggplot(df, aes(DLIndex, colour=Exp, group=Exp)) +
-  geom_density(size=1) +
-  #  scale_colour_manual(values = c("Model" = "#999999", "Full data" = "#E69F00", "Only unicorn absent" = "#56B4E9")) +  
-  scale_y_continuous(limits = c(0, 6)) + 
-  labs(color = "Source of data") +
-  theme_bw()  +
-  ggtitle('Groups of 10') +
-  theme(legend.position="none")
-
-g10
-
-###############################################
 # 50 dyads simulated
 ###############################################
-df1 = read.csv("../Python Codes/Sweeps/sim50_1.csv")
+df1 = read.csv("../Python Codes/Sweeps/Full/sim50_1.csv")
 df1$Exp <- as.character("1")
 head(df1)
-df2 = read.csv("../Python Codes/Sweeps/sim50_2.csv")
+df2 = read.csv("../Python Codes/Sweeps/Full/sim50_2.csv")
 df2$Exp <- as.character("2")
 head(df2)
-df3 = read.csv("../Python Codes/Sweeps/sim50_3.csv")
+df3 = read.csv("../Python Codes/Sweeps/Full/sim50_3.csv")
 df3$Exp <- as.character("3")
 head(df3)
-df4 = read.csv("../Python Codes/Sweeps/sim50_4.csv")
+df4 = read.csv("../Python Codes/Sweeps/Full/sim50_4.csv")
 df4$Exp <- as.character("4")
 head(df4)
-df5 = read.csv("../Python Codes/Sweeps/sim50_5.csv")
+df5 = read.csv("../Python Codes/Sweeps/Full/sim50_5.csv")
 df5$Exp <- as.character("5")
 head(df5)
 
@@ -215,75 +162,23 @@ g50 <- ggplot(df, aes(DLIndex, colour=Exp, group=Exp)) +
 
 g50
 
+
 ###############################################
-# 100 dyads simulated
+# 200 dyads simulated
 ###############################################
-df1 = read.csv("../Python Codes/Sweeps/sim100_1.csv")
+df1 = read.csv("../Python Codes/Sweeps/Full/sim200_1.csv")
 df1$Exp <- as.character("1")
 head(df1)
-df2 = read.csv("../Python Codes/Sweeps/sim100_2.csv")
+df2 = read.csv("../Python Codes/Sweeps/Full/sim200_2.csv")
 df2$Exp <- as.character("2")
 head(df2)
-df3 = read.csv("../Python Codes/Sweeps/sim100_3.csv")
+df3 = read.csv("../Python Codes/Sweeps/Full/sim200_3.csv")
 df3$Exp <- as.character("3")
 head(df3)
-df4 = read.csv("../Python Codes/Sweeps/sim100_4.csv")
+df4 = read.csv("../Python Codes/Sweeps/Full/sim200_4.csv")
 df4$Exp <- as.character("4")
 head(df4)
-df5 = read.csv("../Python Codes/Sweeps/sim100_5.csv")
-df5$Exp <- as.character("5")
-head(df5)
-
-# Create single data frame with DLIndexes
-df <- rbind(
-  df1[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df2[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df3[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df4[c('Round', 
-        'DLIndex',
-        'Exp')],
-  df5[c('Round', 
-        'DLIndex',
-        'Exp')]
-)
-df$Exp <- as.factor(df$Exp)
-head(df)
-# levels(df$Exp)
-
-# Density plot
-g100 <- ggplot(df, aes(DLIndex, colour=Exp, group=Exp)) +
-  geom_density(size=1) +
-  #  scale_colour_manual(values = c("Model" = "#999999", "Full data" = "#E69F00", "Only unicorn absent" = "#56B4E9")) +  
-  scale_y_continuous(limits = c(0, 6)) + 
-  labs(color = "Source of data") +
-  theme_bw() +
-  ggtitle('Groups of 100') +
-  theme(legend.position="none")
-
-g100
-
-###############################################
-# 100 dyads simulated
-###############################################
-df1 = read.csv("../Python Codes/Sweeps/sim200_1.csv")
-df1$Exp <- as.character("1")
-head(df1)
-df2 = read.csv("../Python Codes/Sweeps/sim200_2.csv")
-df2$Exp <- as.character("2")
-head(df2)
-df3 = read.csv("../Python Codes/Sweeps/sim200_3.csv")
-df3$Exp <- as.character("3")
-head(df3)
-df4 = read.csv("../Python Codes/Sweeps/sim200_4.csv")
-df4$Exp <- as.character("4")
-head(df4)
-df5 = read.csv("../Python Codes/Sweeps/sim200_5.csv")
+df5 = read.csv("../Python Codes/Sweeps/Full/sim200_5.csv")
 df5$Exp <- as.character("5")
 head(df5)
 
@@ -422,7 +317,7 @@ g1000 <- ggplot(df, aes(DLIndex, colour=Exp, group=Exp)) +
   scale_y_continuous(limits = c(0, 6)) + 
   labs(color = "Source of data") +
   theme_bw() +
-  ggtitle('Groups of 500') +
+  ggtitle('Groups of 1000') +
   theme(legend.position="none")
 
 g1000
@@ -432,7 +327,7 @@ g1000
 # Combining plots
 ###############################################
 
-grid.arrange(g1, g5, g10, g50, g100, g200,
+grid.arrange(g1, g5, g50, g200, g500, g1000,
              nrow = 3,
              top='Dispersion of simulated groups')
 
