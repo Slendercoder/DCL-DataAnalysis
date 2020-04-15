95e2c75ea9e2703b4ef5187b32e194fa19920f3d
Edgar <edgar@Edgars-MacBook-Pro.local>
2020-04-15 11:27:57 -0500

    Listo para pruebas fitFRA

--- R codes/fitFRA.R
+++ R codes/fitFRA.R
@@ -7,10 +7,11 @@ library(beepr)
 ###############################################################
 
 #archivo <- "../Python Codes/Simulations/M5_full.csv"
-archivo <- "../Python Codes/Simulations/N1_full.csv"
+#archivo <- "../Python Codes/Simulations/N1_full.csv"
+archivo <- "../Python Codes/Dyads/output-435-261.csv"
 df = read.csv(archivo)
 df <- find_joint_region(df)
-df <- get_FRASims(df)
+#df <- get_FRASims(df)
 df$RegionFULL <- unlist(df$RegionFULL)
 df$RegionGo <- factor(df$RegionGo, levels = regiones)
 head(df)
@@ -22,19 +23,7 @@ beep()
 # Parameter recovery...
 ###############################################################
 
-args1 <- args[1:50, ]
-f <- searchBestFit(args1, 1)
-
-###############################################################
-# Plotting...
-###############################################################
-
-theta <- c(0.001, 0.001, 0.001, 0.001, 500, 500, 32, 500, 500, 0.7)
-params <- para_visualizar(imprimir(theta))
-alpha <- 0.3
-min_score = 0
-max_score = 2
-q <- plot_6panels(archivo)
-regs <- c('ALL', 'DOWN', 'IN')
-p <- plot_FRA_regs(df, regs)
-grid.arrange(q, p, ncol=2, widths=c(2/3, 1/3))
+#args <- args[1:2, ]
+f <- searchBestFit(args, 1)
+print(f)
+beep()
