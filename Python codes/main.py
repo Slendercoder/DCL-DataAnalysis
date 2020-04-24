71fb7a1529037d06dda597fd2e15945bf6f83497
Slendercoder <andradeed78@gmail.com>
2020-04-24 13:51:14 -0500

    Comienzan pruebas con graficos

--- Python codes/main.py
+++ Python codes/main.py
@@ -22,10 +22,11 @@ dyads = 50 # number of dyads
 gameParameters = [p, pl, n, rounds, dyads]
 
 # Model Parameters
-modelParameters = [0.001, 0.001, 0.001, 0.001, 500, 500, 32, 500, 500, 0.7] #PL1
+modelParameters = [0.056, 0.038, 0.01, 0.001, 499.577, 499.451, 2.113, 497.689, 499.938, 0.947] #PL1
+# modelParameters = [0.001, 0.001, 0.001, 0.001, 500, 500, 32, 500, 500, 0.7] #PL1
 # modelParameters += [0.001, 0.001, 0.001, 0.001, 200, 500, 32, 200, 500, 0.7] #PL2
 modelParameters += modelParameters # Both players equal parameters
 # modelParameters += [0, 0, 0, 0.4, 500, 500, 32, 0, 0, 0] #PL2
 
-# RM.standard_simulation(gameParameters, modelParameters)
-RM.sample_variation(gameParameters, modelParameters, "FRA", 100)
+RM.standard_simulation(gameParameters, modelParameters)
+# RM.sample_variation(gameParameters, modelParameters, "FRA", 100)
