8a98fafc92593cff58b929eb73829fbddf2d1369
Slendercoder <andradeed78@gmail.com>
2020-05-19 18:24:06 -0500

    MBiases ready

--- R codes/ConfusionMatrix.R
+++ R codes/ConfusionMatrix.R
@@ -1,26 +1,10 @@
 source("MODELpred.R")
 
-fitModels2Data <- function(archivo) {
-  
-  ###############################################################
-  # Loading and preparing data...
-  ###############################################################
-  
-  print(paste("Loading and preparing data", archivo, "..."))
-  df = read.csv(archivo)
-  df$Region <- df$Category
-  df <- find_joint_region(df)
-  df$RegionFULL <- unlist(df$RegionFULL)
-  df$RegionGo <- factor(df$RegionGo, levels = regiones)
-  print(head(df))
-  args <- getFreqFRA(df, theta)
-  args <- get_FRASims_list(args)
-  print(head(args))
-  beep()
-  
-  ###############################################################
-  # Parameter recovery...
-  ###############################################################
+###############################################################
+# Parameter recovery function
+###############################################################
+
+fitModels2Data <- function(args) {
   
   Trials <- 5
   
@@ -41,7 +25,7 @@ fitModels2Data <- function(archivo) {
   print(cat("FRA dev: ",f_FRA$value))
   imprimir(f_FRA$par)
   cm[3] <- f_FRA$value
-
+  
   return(cm)
   
 } # end fitModels2Data
@@ -51,15 +35,49 @@ fitModels2Data <- function(archivo) {
 model <- c('MBiases', 'WSLS', 'FRA')
 matriz <- data.frame(model)
 
+###############################################################
+# Loading and preparing data...
+###############################################################
+
 archivo <- "../Data/MBiases.csv"
-M1 <- fitModels2Data(archivo)
+print(paste("Loading and preparing data", archivo, "..."))
+df = read.csv(archivo)
+df$Region <- df$Category
+df <- find_joint_region(df)
+df$RegionFULL <- unlist(df$RegionFULL)
+df$RegionGo <- factor(df$RegionGo, levels = regiones)
+print(head(df))
+args <- getFreqFRA(df, theta)
+args <- get_FRASims_list(args)
+print(head(args))
+M1 <- fitModels2Data(args)
 matriz <- cbind(matriz, data.frame(M1))
 
 archivo <- "../Data/WSLS.csv"
+print(paste("Loading and preparing data", archivo, "..."))
+df = read.csv(archivo)
+df$Region <- df$Category
+df <- find_joint_region(df)
+df$RegionFULL <- unlist(df$RegionFULL)
+df$RegionGo <- factor(df$RegionGo, levels = regiones)
+print(head(df))
+args <- getFreqFRA(df, theta)
+args <- get_FRASims_list(args)
+print(head(args))
 M2 <- fitModels2Data(archivo)
 matriz <- cbind(matriz, data.frame(M2))
 
 archivo <- "../Data/FRA.csv"
+print(paste("Loading and preparing data", archivo, "..."))
+df = read.csv(archivo)
+df$Region <- df$Category
+df <- find_joint_region(df)
+df$RegionFULL <- unlist(df$RegionFULL)
+df$RegionGo <- factor(df$RegionGo, levels = regiones)
+print(head(df))
+args <- getFreqFRA(df, theta)
+args <- get_FRASims_list(args)
+print(head(args))
 M3 <- fitModels2Data(archivo)
 matriz <- cbind(matriz, data.frame(M3))
 
